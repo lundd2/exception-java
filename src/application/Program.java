777b5f1e11e2e4b1acb41ba8855fc617266520b1
Lund Modolo <lundjpa@gmail.com>
2023-03-07 17:04:40 -0300

    primeira versao

---
+++ src/application/Program.java
@@ -0,0 +1,51 @@
+package application;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entities.Reservation;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.println("Room number: ");
+		int number = sc.nextInt();
+		System.out.println("Check-in date (dd/MM/yyyy): ");
+		Date checkIn = sdf.parse(sc.next());
+		System.out.println("Check-out date (dd/MM/yyyy): ");
+		Date checkOut = sdf.parse(sc.next());
+		
+		if(!checkOut.after(checkIn)) {
+			System.out.println("error1");
+		}
+		else {
+			Reservation reservation = new Reservation(number, checkIn, checkOut);
+			System.out.println("reservation: " + reservation);
+		
+		System.out.println();
+		System.out.println("update: ");
+		System.out.println("Check-in date (dd/MM/yyyy): ");
+		checkIn = sdf.parse(sc.next());
+		System.out.println("Check-out date (dd/MM/yyyy): ");
+		checkOut = sdf.parse(sc.next());
+		
+	
+		String error = reservation.updateDates(checkIn, checkOut);
+		if (error != null) {
+		System.out.println("error reservation: " + error);
+		}
+		else {
+			System.out.println("reservation: " + reservation);
+		}
+	
+		}
+	sc.close();
+	
+}
+}
